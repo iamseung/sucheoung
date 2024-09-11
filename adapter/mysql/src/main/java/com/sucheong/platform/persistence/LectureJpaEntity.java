3fdb313ca67b4715bf8500e1923d7aa6d0b8b84e
iamseung <seesungseok@naver.com>
2024-09-11 13:44:19 +0900

    [2024-09-11] 기본 엔티티 생성
    - 기본 3개에 대한 엔티티 생성
    - 의존성 적용
    - API 서버 구동하면서 엔티티 생성을 위한 EntityScan 적용
    - 변수 설정 + ENV

---
+++ adapter/mysql/src/main/java/com/sucheong/platform/persistence/LectureJpaEntity.java
@@ -0,0 +1,16 @@
+package com.sucheong.platform.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "lecture")
+public class LectureJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private int capacity;
+}
